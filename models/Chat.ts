8ff386425f8a0dfed4e83424d64e7dfa0678ada3
learn-dumboo24 <sanketjha160@gmail.com>
2025-12-01 12:51:36 +0530

    removed unnecessary syntax

--- models/Chat.ts
+++ models/Chat.ts
@@ -25,16 +25,3 @@ const chatSchema = new Schema<IChat>({
 export const Chat = model<IChat>("Chat",chatSchema);
 
 
-// model Chat {
-//   chatId String @id @default(auto()) @map("_id") @db.ObjectId
-//   role String // user or assistant
-//   text String 
-//   chatLabel String @db.ObjectId
-
-//   parentId String? @db.ObjectId
-
-//   userId String @db.ObjectId
-//   user User @relation(fields: [userId], references: [userId])
-  
-//   createdAt DateTime @default(now())
-// }

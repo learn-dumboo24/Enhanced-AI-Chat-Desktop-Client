2cac2d5c13d2a3d59c6cb7eb4ca17cefd7f51f12
learn-dumboo24 <sanketjha160@gmail.com>
2025-12-02 02:22:19 +0530

    Added validation middleware

---
+++ src/middleware/validation.ts
@@ -0,0 +1,11 @@
+import type { Context, Next } from 'hono';
+
+const validation = async(c: Context, next: Next) => {
+    const {email, password} = await c.req.json()
+    if (!email || !email.trim() || !password || !password.trim()){
+        return c.text('All fields are required', 401);
+    }
+    await next()
+}
+
+export default validation;
